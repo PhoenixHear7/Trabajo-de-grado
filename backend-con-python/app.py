130cb5dc7e717374df1fcc0bd34b47b5b892c671
davidvm25 <david2velasquez0@gmail.com>
2025-03-13 08:50:32 -0500

    se agrega carpeta de static para ver el modulo de veterinarios en la red local

--- backend-con-python/app.py
+++ backend-con-python/app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.turnos_routes import turnos_bp
 from routes.veterinarios_routes import veterinarios_bp
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 CORS(app)
 
 # Registrar las rutas
-app.register_blueprint(turnos_bp, )
+app.register_blueprint(turnos_bp)
 app.register_blueprint(veterinarios_bp, url_prefix="/veterinarios")
 
+@app.route("/moduloMedico")
+def modulo_medico():
+    return render_template("moduloMedico.html")
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
